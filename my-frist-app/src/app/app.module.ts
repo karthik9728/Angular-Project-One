2b98453fb5f89f7d22328de9a78c4db40f0c55dc
Karthik <56579449+karthik9728@users.noreply.github.com>
2022-04-28 16:55:21 +0530

    Databinding, Interpolation, Property and Event Binding

--- my-frist-app/src/app/app.module.ts
+++ my-frist-app/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './Server/Server.Component';
+import { ServersComponent } from './servers/servers.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule
---
+++ my-frist-app/src/app/servers/servers.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  allowNewServer = false;
+  serverCreationStatus = 'no server was created'
+
+  constructor() {
+    setTimeout(()=>{
+      this.allowNewServer = true;
+    },3000);
+   }
+
+  ngOnInit(): void {
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus = 'Server was Created'
+  }
+
+}
